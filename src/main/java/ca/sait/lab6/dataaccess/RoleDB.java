cd366a96428d96e67e5015ca25996bf01153b4b9
alexanderyee06 <alexander_yee06@hotmail.com>
2022-03-11 19:55:21 -0700

    Created services

--- src/main/java/ca/sait/lab6/dataaccess/RoleDB.java
+++ src/main/java/ca/sait/lab6/dataaccess/RoleDB.java
@@ -1,7 +1,6 @@
 package ca.sait.lab6.dataaccess;
 
 import ca.sait.lab6.models.Role;
-import ca.sait.lab6.models.User;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -11,30 +10,25 @@ import java.util.List;
 
 public class RoleDB {
 
-    public List<User> getAll() throws Exception {
-        List<User> users = new ArrayList<>();
+    public List<Role> getAll() throws Exception {
+        List<Role> roles = new ArrayList<>();
         ConnectionPool cp = ConnectionPool.getInstance();
         Connection con = cp.getConnection();
         PreparedStatement ps = null;
         ResultSet rs = null;
         
-        String sql = "SELECT * FROM user INNER JOIN role ON role.role_id = user.role";
+        String sql = "SELECT * FROM role";
         
         try {
             ps = con.prepareStatement(sql);
             rs = ps.executeQuery();
             while (rs.next()) {
-                String email = rs.getString(1);
-                boolean active = rs.getBoolean(2);
-                String firstName = rs.getString(3);
-                String lastName = rs.getString(4);;
-                String password = rs.getString(5);;
-                int roleId = rs.getInt(6);
-                String roleName = rs.getString(7);
+                int id = rs.getInt(1);
+                String name = rs.getString(2);
 
-                Role role = new Role(roleId, roleName);
-                User user = new User(email, active, firstName, lastName, password, role);
-                users.add(user);
+                Role role = new Role(id, name);
+
+                roles.add(role);
             }
         } finally {
             DBUtil.closeResultSet(rs);
@@ -42,108 +36,7 @@ public class RoleDB {
             cp.freeConnection(con);
         }
 
-        return users;
+        return roles;
     }
-
-    public User get(String email) throws Exception {
-        User user = null;
-        ConnectionPool cp = ConnectionPool.getInstance();
-        Connection con = cp.getConnection();
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        String sql = "SELECT * FROM user INNER JOIN role ON role.role_id = user.role WHERE email = ?";
-        
-        try {
-            ps = con.prepareStatement(sql);
-            ps.setString(1, email);
-            rs = ps.executeQuery();
-            if (rs.next()) {
-                boolean active = rs.getBoolean(2);
-                String firstName = rs.getString(3);
-                String lastName = rs.getString(4);;
-                String password = rs.getString(5);;
-                int roleId = rs.getInt(6);
-                String roleName = rs.getString(7);
-
-                Role role = new Role(roleId, roleName);
-                user = new User(email, active, firstName, lastName, password, role);
-            }
-        } finally {
-            DBUtil.closeResultSet(rs);
-            DBUtil.closePreparedStatement(ps);
-            cp.freeConnection(con);
-        }
-        
-        return user;
-    }
-
-    public boolean insert(User user) throws Exception {
-        ConnectionPool cp = ConnectionPool.getInstance();
-        Connection con = cp.getConnection();
-        PreparedStatement ps = null;
-        String sql = "INSERT INTO 'user' ('email', 'first_name', 'last_name', 'password', 'role') VALUES (?, ?, ?, ?, ?)";
-        
-        boolean inserted = false;
-
-        try {
-            ps = con.prepareStatement(sql);
-            ps.setString(1, user.getEmail());
-            ps.setString(2, user.getFirstName());
-            ps.setString(3, user.getLastName());
-            ps.setString(4, user.getPassword());
-            ps.setInt(5, user.getRole().getRoleId());
-
-            inserted = ps.executeUpdate() != 0;
-        } finally {
-            DBUtil.closePreparedStatement(ps);
-            cp.freeConnection(con);
-        }
-
-        return inserted;
-    }
-
-    public boolean update(User user) throws Exception {
-        ConnectionPool cp = ConnectionPool.getInstance();
-        Connection con = cp.getConnection();
-        PreparedStatement ps = null;
-        String sql = "UPDATE user SET , 'first_name' = ?, 'last_name' = ?, 'password' = ?, 'role' = ? WHERE 'email' = ?";
-        
-        boolean updated;
-
-        try {
-            ps = con.prepareStatement(sql);
-            ps.setString(1, user.getFirstName());
-            ps.setString(2, user.getLastName());
-            ps.setString(3, user.getPassword());
-            ps.setInt(4, user.getRole().getRoleId());
-            ps.setString(5, user.getEmail());
-            updated = ps.executeUpdate() != 0;
-        } finally {
-            DBUtil.closePreparedStatement(ps);
-            cp.freeConnection(con);
-        }
-
-        return updated;
-    }
-
-    public boolean delete(User user) throws Exception {
-        ConnectionPool cp = ConnectionPool.getInstance();
-        Connection con = cp.getConnection();
-        PreparedStatement ps = null;
-        String sql = "DELETE FROM user WHERE email = ?";
-
-        boolean deleted;
-        
-        try {
-            ps = con.prepareStatement(sql);
-            ps.setString(1, user.getEmail());
-            deleted = ps.executeUpdate() != 0;
-        } finally {
-            DBUtil.closePreparedStatement(ps);
-            cp.freeConnection(con);
-        }
-
-        return deleted;
-    }
-
 }
+
--- src/main/java/ca/sait/lab6/dataaccess/UserDB.java
+++ src/main/java/ca/sait/lab6/dataaccess/UserDB.java
@@ -146,4 +146,8 @@ public class UserDB {
         return deleted;
     }
 
+    public List<User> getAll(String email) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
 }
